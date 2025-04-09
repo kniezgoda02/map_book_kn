2dcd96d238223c0878797201227b3eca474fa675
kniezgoda02 <klaudia.niezgoda@student.wat.edu.pl>
2025-04-09 13:28:58 +0200

    wip: add menu

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ from utils.controller import get_user_info
 
 def main():
     print(f'Wiataj {users[0]['name']}')
-    get_user_info(users[1:])
+    while True:
+        print('============MENU===============')
+        print('0 - Zakończ program')
+        print('1 - Wyświetl znajomych')
+        print('2 - Dodaj znajomego')
+        print('===============================')
+
+        choice = input('Wybierz opcje Menu')
+        if choice == '0': break
+        if choice == '1': get_user_info(users[1:])
 
 
 if __name__ == '__main__':
