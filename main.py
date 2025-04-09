4350a29f5c10b8981f9021e38ec4aa3eb13ba050
kniezgoda02 <klaudia.niezgoda@student.wat.edu.pl>
2025-04-09 12:48:06 +0200

    wip: data structure development

---
+++ main.py
@@ -0,0 +1,31 @@
+users: list= [
+    {'name': 'Julia', 'location': 'Ząbki', 'posts': 10},
+    {'name': 'Julia', 'location': 'Sokółka', 'posts': 20},
+    {'name': 'Klaudia', 'location': 'Warszawa', 'posts': 15},
+    {'name': 'Marcin', 'location': 'Grudziądź', 'posts': 1000},
+    {'name': 'Mateusz', 'location': 'Lublin', 'posts': 100},
+]
+
+
+
+
+
+def get_user_info(users_data: list)->None:
+    for user in users_data:
+        print(f'Twój znajomy {user['name']} z miejscowości {user ['location']} opublikowało {user["posts"]}')
+
+
+print(f'Wiataj {users[0]['name']}')
+get_user_info(users[1:])
+
+
+
+
+
+
+
+
+
+
+
+
